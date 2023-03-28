4bad10b21c5eadf94a878d77ef8edb4e44ea2348
shoaibmehmood243 <shoaibmehmood065@gmail.com>
2023-03-29 01:08:47 +0500

    form fixes

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = ()=> {
         <div className='surface-900 text-white text-sm py-1 pr-8 banner'>
             <div className='flex justify-content-end align-items-center gap-1'>
                 <img src={phoneCall} />
-                <span><a href="tel:+551 225 0733">+551 225 0733</a></span>
+                <span><a href="tel:+551 225 0733">551 225 0733</a></span>
             </div>
         </div>
     )
